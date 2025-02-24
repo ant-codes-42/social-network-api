d7ed2aee54c33fe0451d7c3e3ade6071e0291115
ant-codes-42 <schwab.it@pm.me>
2025-02-24 15:58:16 -0600

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express, { Application } from 'express';
+import mongoose, { ConnectOptions } from 'mongoose';
+
+// Connect to MongoDB
+const app: Application = express();
+const PORT: number | string = process.env.PORT || 3001;
+const MONGO_URI: string = 'mongodb://127.0.0.1:27017/socialNetwork';
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+    family: 4,
+    serverSelectionTimeoutMS: 5000,
+} as ConnectOptions)
+.then(() => console.log('MongoDB connected'))
+.catch((err: Error) => console.error('MongoDB connection error:', err));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
